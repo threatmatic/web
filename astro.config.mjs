1902d3fb43ff5bb3ae14f2b4ca64a63483a972d9
ulagappan <ulagappan.maeiyappan@e-cargoware.com>
2025-06-15 10:41:17 +0530

    url change did't work on navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://SriValliammai.github.io/threatmatic/',
+  site: 'https://SriValliammai.github.io',
+  base: '/threatmatic/',
   vite: {
     plugins: [tailwindcss()],
     
