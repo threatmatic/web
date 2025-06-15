0ce89989b4697ee662ee0cca391b21ed497c3797
ulagappan <ulagappan.maeiyappan@e-cargoware.com>
2025-06-16 00:51:40 +0530

    adding site and base on config page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://threatmatic.github.io/web',
+  base: '/web/',
   vite: {
     plugins: [tailwindcss()],
     
