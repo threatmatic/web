4e0883d3da7695987482cd5177499fb570427088
ulagappan <ulagappan.maeiyappan@e-cargoware.com>
2025-06-15 10:48:43 +0530

    testurl 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://SriValliammai.github.io//threatmatic',
+  site: 'https://SriValliammai.github.io/threatmatic',
   base: '/threatmatic/',
   vite: {
     plugins: [tailwindcss()],
